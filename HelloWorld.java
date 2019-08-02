d283e6e7ceb05e7d8c78ee90fc38ad447d22db5c
陈治远 <291523820@qq.com>
2019-08-02 14:15:24 +0800

    edit by 陈治远

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("HelloWorld");
 		System.out.printlnI("update by ChuanA68886");
+		System.out.println("edit by 陈治远");
 	}
 }
