94d9c1e5a371e933e001bb8b61042dd8decaaf81
ChuanA68886 <ChenZhiYuan_CD@aliyun.com>
2019-08-02 14:34:30 +0800

    edit by ChuanA68886

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("HelloWorld");
-		System.out.printlnI("update by ChuanA68886");
+		System.out.println("update by ChuanA68886");
 		System.out.println("edit by 陈治远");
+		System.out.println("123456");
 	}
 }
